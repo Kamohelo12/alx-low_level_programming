ea1628ce398f1db51008ca8378e2e4a7ae19a060
Qelemoze Kamohelo Sikhosana <sikhosanaqelemoze@gmail.com>
2022-12-14 19:23:17 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alpharbet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}	
